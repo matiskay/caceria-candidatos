4d196fec5914be06443072e2e242fab9579a426b
Edgar Marca <matiskay@gmail.com>
2016-01-10 10:35:17 -0500

    Minor moficiation in search_from_list_of_names.py script

--- search_from_list_of_names.py
+++ search_from_list_of_names.py
@@ -2,22 +2,24 @@ import argparse
 
 from elasticsearch import Elasticsearch
 
-
-def search(input_filename):
+def search(name):
     es = Elasticsearch()
+    res = es.search(index="great_db", q=name)
+
+    for i in res['hits']['hits']:
+        if i['_score'] >= 2.0:
+            out = u"{0}\t{1}\t{2}\t{3}".format(
+                    i['_score'], i['_source']['source'], name, i['_source']['raw_data'],
+            )
+            print out
+
+def search_list(input_filename):
     with open(input_filename, "r") as handle:
         names = handle.readlines()
 
-    for line in names:
-        name = line.strip()
-        res = es.search(index="great_db", q=name)
-        for i in res['hits']['hits']:
-            if i['_score'] >= 2.0:
-                out = u"{0}\t{1}\t{2}\t{3}".format(
-                    i['_score'], i['_source']['source'], name, i['_source']['raw_data'],
-                )
-                print (out)
-
+    for name in names:
+        name = name.strip()
+        search(name)
 
 def main():
     parser = argparse.ArgumentParser(description="Do a search of person's name "
@@ -29,7 +31,7 @@ def main():
     args = parser.parse_args()
     input_filename = args.input_file
     if input_filename:
-        search(input_filename)
+        search_list(input_filename)
 
 
 if __name__ == "__main__":
